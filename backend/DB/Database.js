edcccc0a9e7296343c960de78d33134df6deca5c
devdeepak06 <k.deepak2217@gmail.com>
2024-12-26 17:13:44 +0530

    initial commit

---
+++ backend/DB/Database.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    // const db = process.env.MONGO_URL;
+    const db = 'mongodb+srv://kdeepak2217:FyugB0N6kX0E0Rhh@cluster0.z8vws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+    if (!db) {
+        throw new Error("MONGO_URL is not defined in the environment variables");
+    }
+
+    try {
+        const { connection } = await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true, // Ensures support for the latest MongoDB driver
+        });
+
+        console.log(`MongoDB Connected: ${connection.host}`);
+    } catch (error) {
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+        process.exit(1); // Exit the process with failure
+    }
+};
