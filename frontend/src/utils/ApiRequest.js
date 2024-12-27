8c19a5004bf8c781d5736c7fe106589a70d14445
devdeepak06 <k.deepak2217@gmail.com>
2024-12-27 18:22:34 +0530

    update backend/userRouter.js

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -7,3 +7,13 @@ export const addTransaction = `${host}/api/v1/addTransaction`;
 export const getTransactions = `${host}/api/v1/getTransaction`;
 export const editTransactions = `${host}/api/v1/updateTransaction`;
 export const deleteTransactions = `${host}/api/v1/deleteTransaction`;
+
+export async function apiRequest(url, options = {}) {
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer9ghjV3#F5x!5Lkksd8d$R#d9a%4&FJ8aKd`
+  };
+
+  const response = await fetch(url, { ...options, headers });
+  return response.json();
+}
